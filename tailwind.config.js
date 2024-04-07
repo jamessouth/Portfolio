187c1dbc37fc2d46ff5d2ff0ce51c07e38735dd4
jamessouth <jamesdanielsouth@gmail.com>
2024-04-07 01:51:40 -0500

    move to rescript

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+// module.exports = {
+//     content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+//     theme: {
+//       extend: {},
+//     },
+//     plugins: [],
+//   };
+  
+
 module.exports = {
     future: {
         purgeLayersByDefault: true,
